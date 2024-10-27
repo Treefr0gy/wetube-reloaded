92b28aa462fa6373b1708dc84c4d58e9e4ed6053
이준성 <109706353+Treefr0gy@users.noreply.github.com>
2024-10-27 13:41:29 +0900

    Register View Event

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -107,12 +107,20 @@ const handleKeyboard = (e) => {
     }
 };
 
+const handleEnded = () => {
+    const { id } = videoContainer.dataset;
+    fetch(`/api/videos/${id}/views`, {
+        method: "POST",
+    });
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadeddata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("click", handlePlayClick);
+video.addEventListener("ended", handleEnded);
 videoContainer.addEventListener("mousemove", handleMouseMove);
 videoContainer.addEventListener("mouseleave", handleMouseLeave);
 timeline.addEventListener("input", handleTimelineChange);
